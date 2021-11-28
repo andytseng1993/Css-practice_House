f06bd4a4397236f65088bce7e76975fc9368445c
Andy Tseng <anby1611@gmail.com>
2021-11-28 10:32:29 +0000

    night sky

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ let morning = document.querySelector('.sun_sky .morning')
 morning.innerHTML= morning.textContent.split('').map((char,i)=>
     `<span style='animation-delay: calc(0.2s*${i+1}),0s;'>${char}</span>`
  ).join('')
+
+ let night = document.querySelector('.moon_sky .night')
+ night.innerHTML= night.textContent.split('').map((char,i)=>
+     `<span style='animation-delay: calc(0.2s*${-i-1}),0s;'>${char}</span>`
+  ).join('')
